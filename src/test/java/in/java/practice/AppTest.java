9df4703f523992632bf305fe52b309dfbc47a9be
Pradeep,Kukkala <pradeepprdp58@gmail.com>
2019-03-06 23:16:21 +0530

    first commit

---
+++ src/test/java/in/java/practice/AppTest.java
@@ -0,0 +1,18 @@
+package in.java.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
